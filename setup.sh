aafe7349fbd1dae36d378714e3b34c363136901b
Layne <LayneH@users.noreply.github.com>
2017-05-14 17:37:03 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 # Get CIFAR100
 mkdir -p datasets
 wget -P datasets https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz
-tar -xzvf cifar-100-python.tar.gz
-rm cifar-100-python.tar.gz
+tar -xzvf datasets/cifar-100-python.tar.gz
+rm datasets/cifar-100-python.tar.gz
 
 # Get pretrained VGG Net
 mkdir -p vgg
